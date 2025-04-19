5957da791d8d21d6d8a390753594966c55b37e7f
DRTX2 <davidmanjarres2004@gmail.com>
2025-04-19 00:29:20 -0500

    Create service

--- TodoList/Domain/Ports/In/TaskService.cs
+++ TodoList/Domain/Ports/In/TaskService.cs
@@ -1,10 +1,8 @@
+using TodoList.Entities;
+
 namespace TodoList.Domain.Ports.In;
 
 public interface TaskService
 {
-    Task fetchOne(int id);
-    void Create(int taskList,Task task);
-    void Update(int id, Task task);
-    
-    void Delete(int id);
+    Task<TaskItem> CreateTaskAsync(T)
 }
